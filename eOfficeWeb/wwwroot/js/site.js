8c4007955948f9b650667e516d0eefe6e14b2379
Hari Om Ahlawat <hariomahlawat@gmail.com>
2023-07-10 19:07:50 +0530

    Toastr not working. Lets move ahead.

--- eOfficeWeb/wwwroot/js/site.js
+++ eOfficeWeb/wwwroot/js/site.js
@@ -1,2 +1,17 @@
-﻿$('#tableDatatable').dataTable({
-});
+﻿window.onload = function () {
+    $('#tableDatatable').dataTable();
+
+    var successDiv = document.getElementById('success-message');
+    var errorDiv = document.getElementById('error-message');
+
+    var successMessage = successDiv.dataset.message;
+    var errorMessage = errorDiv.dataset.message;
+
+    if (successMessage) {
+        displayToast('success', successMessage);
+    }
+
+    if (errorMessage) {
+        displayToast('error', errorMessage);
+    }
+};
